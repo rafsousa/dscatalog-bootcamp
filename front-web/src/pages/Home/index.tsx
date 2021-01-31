5939666db2a30ce4607d18a771aec79050c42be7
Rodrigo Alex <rodrigo.alex.sousa@gmail.com>
2021-01-30 21:12:34 -0300

    Page catalog

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -4,11 +4,12 @@ import ButtonIcon from '../../core/components/ButtonIcon';
 import { Link } from 'react-router-dom'
 
 import './styles.scss';
+import '../../app.scss';
 
 const Home = () => (
     <div className="home-container">
-        <div className="row home-content">
-            <div className="col-6">
+        <div className="row home-content ">
+            <div className="col-6 home-text">
                 <h1 className="text-title">Conheça o melhor <br /> catálogo de produtos</h1>
                 <p className="text-subtitle">
                     Ajudaremos você a encontrar os melhores <br /> produtos disponíveis no mercado.
