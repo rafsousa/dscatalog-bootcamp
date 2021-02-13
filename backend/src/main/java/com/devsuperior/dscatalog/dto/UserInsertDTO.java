91bbdd987c3b1aa1538187eb866f96494bb79b2e
rafsousa <rodrigo.alex.sousa@gmail.com>
2021-02-13 20:49:53 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.dto;
 
 public class UserInsertDTO extends UserDTO {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String password;
 	
 	public UserInsertDTO() {
